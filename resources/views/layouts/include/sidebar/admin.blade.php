ae264dc7b469aa58de21511bac6a23dbd8c5322a
Abdurahman Zuhair <abdurrahman.zuh@gmail.com>
2024-04-26 14:56:29 +0700

    update

--- resources/views/layouts/include/sidebar/admin.blade.php
+++ resources/views/layouts/include/sidebar/admin.blade.php
@@ -1,5 +1,7 @@
 <aside id="sidebar" class="sidebar">
+
     <ul class="sidebar-nav" id="sidebar-nav">
+
         <li class="nav-item">
             <a class="nav-link " href="{{ route('admin.dashboard') }}">
                 <i class="bi bi-grid"></i>
@@ -8,7 +10,7 @@
         </li><!-- End Dashboard Nav -->
 
         <li class="nav-item">
-            <a class="nav-link {{ request()->routeIs('admin.category.*') ? 'collapsed' : '' }}" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <a class="nav-link {{ request()->routeIs('admin.category.*') ? '' : 'collapsed' }}" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
                 <i class="bi bi-menu-button-wide"></i><span>Product</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
             <ul id="components-nav" class="nav-content collapse {{ request()->routeIs('admin.category.*') ? 'show' : '' }}" data-bs-parent="#sidebar-nav">
@@ -16,13 +18,14 @@
                     <a href="{{ route('admin.category.index') }}" class="{{ request()->routeIs('admin.category.index') ? 'active' : '' }}">
                         <i class="bi bi-circle"></i><span>Category</span>
                     </a>
-            </ul>
-            <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                </li>
                 <li>
-                    <a href="">
-                        <i class="bi bi-circle"></i><span>Data Product</span>
+                    <a href="components-alerts.html">
+                        <i class="bi bi-circle"></i><span>Product</span>
                     </a>
+                </li>
             </ul>
         </li><!-- End Components Nav -->
     </ul>
+
 </aside>
