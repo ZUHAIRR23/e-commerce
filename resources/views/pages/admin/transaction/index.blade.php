771177c1b0078d50fd5fc0dc77b73a436bf0045b
Abdurahman Zuhair <abdurrahman.zuh@gmail.com>
2024-05-22 15:01:48 +0700

    p

---
+++ resources/views/pages/admin/transaction/index.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.parent')
+
+@section('title', 'Transaction')
+
+@section('content')
+
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Transaction</h5>
+
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                    <li class="breadcrumb-item"><a href="#">Transaction</a></li>
+                    <li class="breadcrumb-item active">Transaction</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-body">
+            <div class="card-title"><i class="bi bi-cart"></i> List Transaction </div>
+
+            <table class="table table-striped table-hover table-bordered datatable">
+                <thead>
+                    <tr>
+                        <td>No</td>
+                        <td>Name Account</td>
+                        <td>Reciever Name</td>
+                        <td>Email</td>
+                        <td>Phone</td>
+                        <td>Total Price</td>
+                        <td>Status</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($transaction as $row)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ auth()->user()->name }}</td>
+                            <td>{{ $row->name }}</td>
+                            <td>{{ $row->email }}</td>
+                            <td>{{ $row->phone }}</td>
+                            <td>
+                                @if ($row->status == 'EXPIRED')
+                                    <span class="badge bg-danger">Expired</span>
+                                @elseif ($row->status == 'PENDING')
+                                    <span class="badge bg-warning">Pending</span>
+                                @elseif ($row->status == 'SETTLEMENT')
+                                    <span class="badge bg-info">Settlement</span>
+                                @else
+                                    <span class="badge bg-success">Success</span>
+                                @endif
+                            </td>
+                            <td>{{ $row->total_price }}</td>
+                            <td>
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                    data-bs-target="#ExtralargeModal">
+                                    <i class="bi bi-eye"></i>
+                                </button>
+                                @include('pages.admin.my-transaction.show')
+                            </td>
+                        </tr>
+                    @empty
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+@endsection
